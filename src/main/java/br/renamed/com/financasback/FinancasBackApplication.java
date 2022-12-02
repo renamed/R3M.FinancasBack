f70162c770b8cc023e77cc03ea90c6ca438ebe01
Renato Moura Martins Medeiros <renamedrj@gmail.com>
2022-12-02 16:59:41 -0300

    Initial commit

---
+++ src/main/java/br/renamed/com/financasback/FinancasBackApplication.java
@@ -0,0 +1,13 @@
+package br.renamed.com.financasback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancasBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancasBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/renamed/com/financasback/FinancasBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.renamed.com.financasback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
