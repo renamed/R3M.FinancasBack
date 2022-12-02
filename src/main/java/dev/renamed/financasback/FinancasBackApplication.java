0880cb8936644ed6d0042c093e9e6c824abd034d
Renato Moura Martins Medeiros <renamedrj@gmail.com>
2022-12-02 18:37:36 -0300

    Database connection properties

--- src/main/java/dev/renamed/financasback/FinancasBackApplication.java
+++ src/main/java/dev/renamed/financasback/FinancasBackApplication.java
@@ -1,4 +1,4 @@
-package renamed.dev.financasback;
+package dev.renamed.financasback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/dev/renamed/financasback/FinancasBackApplicationTests.java
+++ src/test/java/dev/renamed/financasback/FinancasBackApplicationTests.java
@@ -1,4 +1,4 @@
-package renamed.dev.financasback;
+package dev.renamed.financasback;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
